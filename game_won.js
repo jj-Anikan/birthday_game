ab506af7067eba2c5e958e24548ff4b2fcf743be
Jacob Mayhew <105332338+jj-Anikan@users.noreply.github.com>
2023-10-28 15:19:48 -0400

    Update game_won.js

--- game_won.js
+++ game_won.js
@@ -18,5 +18,14 @@ function createConfetti() {
   confetti.style.left = Math.random() * 100 + "vw"; // Random horizontal position
   confetti.style.animation = `fall 3s linear ${Math.random() * 2 + 1}s infinite`;
 
+  // Create an anchor element with a link inside the confetti
+  const confettiLink = document.createElement("a");
+  confettiLink.href = "https://example.com"; // Replace with your desired link
+  confettiLink.target = "_blank"; // Open link in a new tab
+  confettiLink.innerText = "Click me!"; // Text for the link
+
+  // Append the link to the confetti
+  confetti.appendChild(confettiLink);
+
   document.body.appendChild(confetti);
 }
