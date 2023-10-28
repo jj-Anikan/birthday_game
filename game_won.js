63692e480739413d75bd3b69e537d73f002037f5
jjmayhew27 <jjmayhew27@gmail.com>
2023-10-28 15:30:22 -0400

    Make changes

--- game_won.js
+++ game_won.js
@@ -18,14 +18,5 @@ function createConfetti() {
   confetti.style.left = Math.random() * 100 + "vw"; // Random horizontal position
   confetti.style.animation = `fall 3s linear ${Math.random() * 2 + 1}s infinite`;
 
-  // Create an anchor element with a link inside the confetti
-  const confettiLink = document.createElement("a");
-  confettiLink.href = "https://www.facebook.com/coogans.nyc/videos/lmm-singing-happy-birthday-last-night/1541656815889420/";
-  confettiLink.target = "_blank"; // Open link in a new tab
-  confettiLink.innerText = "Click me!"; // Text for the link
-
-  // Append the link to the confetti
-  confetti.appendChild(confettiLink);
-
   document.body.appendChild(confetti);
 }
